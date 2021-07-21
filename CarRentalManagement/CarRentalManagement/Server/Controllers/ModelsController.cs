e84dcb345944a37003d11e7467bc5cf854d0acb9
LeeTrent <lee.trent.1@gmail.com>
2021-07-21 10:06:23 -0500

    Adds ModelsController

---
+++ CarRentalManagement/CarRentalManagement/Server/Controllers/ModelsController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Shared.Domain;
+using CarRentalManagement.Server.IRepository;
+using System.Collections.Generic;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    ////[Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    public class ModelsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ModelsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: /Models
+        [HttpGet]
+        public async Task<IActionResult> GetModels()
+        {
+            IList<Model> models = await _unitOfWork.Models.GetAll();
+
+            if (models == null || models.Count < 1)
+            {
+                return NotFound();
+            }
+            return Ok(models);
+        }
+
+        // GET: /Models/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetModel(int id)
+        {
+            Model model = await _unitOfWork.Models.Get(q => q.Id == id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+
+        // PUT: /Models/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutModel(int id, Model model)
+        {
+            if (id != model.Id)
+            {
+                return BadRequest();
+            }
+
+            _unitOfWork.Models.Update(model);
+
+            try
+            {
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await ModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: /Models
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Make>> PostModel(Model model)
+        {
+            await _unitOfWork.Models.Insert(model);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetMake", new { id = model.Id }, model);
+        }
+
+        // DELETE: Models/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteModel(int id)
+        {
+            Model model = await _unitOfWork.Models.Get(q => q.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            await _unitOfWork.Models.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        private async Task<bool> ModelExists(int id)
+        {
+            Model model = await _unitOfWork.Models.Get(q => q.Id == id);
+            return model == null;
+        }
+    }
+}
--- CarRentalManagement/CarRentalManagement/Server/IRepository/IUnitOfWork.cs
+++ CarRentalManagement/CarRentalManagement/Server/IRepository/IUnitOfWork.cs
@@ -11,7 +11,7 @@ namespace CarRentalManagement.Server.IRepository
     {
         Task Save(HttpContext httpContext);
         IGenericRepository<Make> Makes { get; }
-        //IGenericRepository<Model> Models { get; }
+        IGenericRepository<Model> Models { get; }
         //IGenericRepository<Vehicle> Vehicles { get; }
         IGenericRepository<Colour> Colours { get; }
         //IGenericRepository<Booking> Bookings { get; }
