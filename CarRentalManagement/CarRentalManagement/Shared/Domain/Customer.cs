c5c6a1280381fee6b4aa1c1d79d498324b146b86
LeeTrent <lee.trent.1@gmail.com>
2021-08-09 08:24:38 -0500

    Adds first and last name to Customer

--- CarRentalManagement/CarRentalManagement/Shared/Domain/Customer.cs
+++ CarRentalManagement/CarRentalManagement/Shared/Domain/Customer.cs
@@ -4,6 +4,8 @@ namespace CarRentalManagement.Shared.Domain
 {
     public class Customer : BaseDomainModel
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string TaxId { get; set; }
         public string Address { get; set; }
         public string ContactNumber { get; set; }
