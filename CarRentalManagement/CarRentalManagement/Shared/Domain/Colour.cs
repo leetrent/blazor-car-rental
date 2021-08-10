8c406949ce8d708496b11729ab857419a28a38fd
LeeTrent <lee.trent.1@gmail.com>
2021-08-10 07:49:11 -0500

    Makes Colour.Name a required field with minimum length of 3

--- CarRentalManagement/CarRentalManagement/Shared/Domain/Colour.cs
+++ CarRentalManagement/CarRentalManagement/Shared/Domain/Colour.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace CarRentalManagement.Shared.Domain
 {
     public class Colour : BaseDomainModel
     {
+        [Required(ErrorMessage = "Please enter a colour name.")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Colour name must contain at least 3 characters.")]
         public string Name { get; set; }
     }
 }
