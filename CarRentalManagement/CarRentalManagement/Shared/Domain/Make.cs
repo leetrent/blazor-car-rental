749bd14f56a4e2153ab1e52b6e4a7c64b69a2ffd
LeeTrent <lee.trent.1@gmail.com>
2021-08-10 20:14:58 -0500

    Make Make name a required field

--- CarRentalManagement/CarRentalManagement/Shared/Domain/Make.cs
+++ CarRentalManagement/CarRentalManagement/Shared/Domain/Make.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CarRentalManagement.Shared.Domain
 {
     public class Make : BaseDomainModel
     {
+        [Required(ErrorMessage = "Please enter a make name.")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Colour name must contain at least 2 characters.")]
         public string Name { get; set; }
     }
 }
